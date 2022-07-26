f9697c97f8e0b1e0337683eb2b439131a70da9fb
yasirakram <yasir311997@gmail.com>
2022-07-26 17:47:32 +0500

    ch

--- register.php
+++ register.php
@@ -7,14 +7,12 @@
 	<title>RegistrationForm_v2 by Colorlib</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<!-- MATERIAL DESIGN ICONIC FONT -->
-	<link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
-
+	 
 	<!-- STYLE CSS -->
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class="wrapper" style="background-image: url('images/img2.jpg');">
+	<div class="wrapper" style="background-image: url('assest/img2.jpg');">
 		<div class="inner">
 			<form method="post" action="register.php">
 				<h3>Register</h3>
