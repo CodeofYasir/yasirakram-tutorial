e363aeee7882f66b9121c6a307ea89bc6d07e4c5
yasirakram <yasir311997@gmail.com>
2022-07-26 15:57:42 +0500

    ch

--- login.php
+++ login.php
@@ -1,18 +1,18 @@
-<?php include('server.php') ?>
- 
+<!DOCTYPE html>
 <html lang="en">
-<head>
+	<head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+	
 	<!-- STYLE CSS -->
 	<link rel="stylesheet" href="./css/style.css">
-
+	
 	<title>Manage Khata</title>
 </head>
 <body>
 	
+	<?php include('server.php')?>
 	<div class="wrapper" style="background-image: url('assest/img2.jpg');">
 		<div class="inner">
  
