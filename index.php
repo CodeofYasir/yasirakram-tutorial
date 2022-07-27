9670b92ea4b30454c8c4b17393bc944246799fe3
yasirakram <yasir311997@gmail.com>
2022-07-27 13:27:43 +0500

    ch

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php include('server.php') ?>
 <?php  
-  if (!isset($_SESSION['username'])) {
+  if (!isset($_SESSION['email'])) {
 	  $_SESSION['msg'] = "You must log in first";
 	  
   	header('location: login.php');
 }
 if (isset($_GET['logout'])) {
 	session_destroy();
-	unset($_SESSION['username']);
+	unset($_SESSION['email']);
 	header("location: login.php");
 }
 $query = "SELECT * FROM lender WHERE u_id = '$u_id'";  
