b2ef300b8605c4a1ed78a1d855dcf074065548b2
yasirakram <yasir311997@gmail.com>
2022-07-27 13:23:31 +0500

    ch

--- server.php
+++ server.php
@@ -69,10 +69,10 @@ if (isset($_POST['reg_user'])) {
 // LOGIN USER
 
 if (isset($_POST['login_user'])) {
-    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
   
-    if (empty($username)) {
+    if (empty($email)) {
         array_push($errors, "Username is required");
     }
     if (empty($password)) {
@@ -80,10 +80,10 @@ if (isset($_POST['login_user'])) {
     }  
     if (count($errors) == 0) {
         $password = md5($password);
-        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
-          $_SESSION['username'] = $username;
+          $_SESSION['email'] = $email;
           $_SESSION['success'] = "You are now logged in";
           header('location: index.php');
         }else {
