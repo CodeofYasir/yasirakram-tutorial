1d4e482c692edb9ca51afae45931653bc7073bf5
yasirakram <yasir311997@gmail.com>
2022-07-30 07:59:07 +0500

    ch

--- generate_pdf.php
+++ generate_pdf.php
@@ -36,9 +36,9 @@
         $query_builder = TRUE;
         // $con = new PDO("mysql:host = $host;dbname=$dbname","$user",$password);
         $pdf->Multicell(40,10,$host,1,0,'C');
-        $pdf->Multicell(40,10,$dbname,1,0,'C');
+        $pdf->cell(40,10,$dbname,1,0,'C');
         $pdf->Multicell(40,10,$user,1,0,'C');
-        $pdf->Multicell(40,10,$password,1,0,'C');
+        $pdf->cell(40,10,$password,1,0,'C');
         
         
          
