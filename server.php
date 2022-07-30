00a6a68dc4d44ae468ce9590be51b73bc1d4e331
yasirakram <yasir311997@gmail.com>
2022-07-30 12:23:09 +0500

    ch

--- server.php
+++ server.php
@@ -2,18 +2,18 @@
 
 session_start();
 
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// $db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
 
-$db = mysqli_connect('localhost', 'root', '', 'khata');
+// $db = mysqli_connect('localhost', 'root', '', 'khata');
 
 
 $username = "";
