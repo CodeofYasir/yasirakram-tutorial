352938decb7ca05ffe14ec41c40cc4ea87d7763e
yasirakram <yasir311997@gmail.com>
2022-07-30 08:26:35 +0500

    ch

--- generate_pdf.php
+++ generate_pdf.php
@@ -37,7 +37,7 @@
 
         $db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-        $con = new PDO("mysql:$cleardb_server;dbname:$cleardb_db",, $cleardb_username,$cleardb_password);
+        $con = new PDO("mysql:$cleardb_server;dbname:$cleardb_db",$cleardb_username,$cleardb_password);
 
 
         $query ="SELECT * FROM expenses WHERE u_id = '$u_id'";
