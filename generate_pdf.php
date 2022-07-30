8dea36f8e5de860c282468c2118446826cae13d3
yasirakram <yasir311997@gmail.com>
2022-07-30 08:22:24 +0500

    ch

--- generate_pdf.php
+++ generate_pdf.php
@@ -27,16 +27,17 @@
         // $user="root";
         // $con = new PDO("mysql:host = $host;dbname=$dbname","$user",$password);
 
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db = substr($cleardb_url["path"],1);
-        $active_group = 'default';
-        $query_builder = TRUE;
-        
-        $dns = "mysql:host=$cleardb_server;dbname= $cleardb_db";
-        $con = new PDO($dns,$cleardb_username,$cleardb_password);
+        // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        // $cleardb_server = $cleardb_url["host"];
+        // $cleardb_username = $cleardb_url["user"];
+        // $cleardb_password = $cleardb_url["pass"];
+        // $cleardb_db = substr($cleardb_url["path"],1);
+        // $active_group = 'default';
+        // $query_builder = TRUE;
+
+        $db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+        $con = new PDO("mysql:$cleardb_server;dbname:$cleardb_username",$cleardb_password, $cleardb_db);
 
 
         $query ="SELECT * FROM expenses WHERE u_id = '$u_id'";
